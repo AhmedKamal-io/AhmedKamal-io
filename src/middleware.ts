d7b08569424f59f5234ea195f56032e17cbba93f
Ahmed Kamal <220857366+AhmedKamal-io@users.noreply.github.com>
2026-01-26 18:53:32 +0200

    Delete middleware.ts

--- src/middleware.ts
+++
@@ -1,9 +0,0 @@
-import createMiddleware from "next-intl/middleware";
-import { routing } from "@/i18n/routing";
-
-// هنا الـ default export اللي لازم Next.js يلاقيه
-export default createMiddleware(routing);
-
-export const config = {
-  matcher: ["/((?!api|_next|_vercel|.*\\..*).*)"],
-};
